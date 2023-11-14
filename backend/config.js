e58e4267e8b5cc1f0deaae27c0c8d1e7247fe94b
RQuicho <raymondquicho@gmail.com>
2023-11-13 21:42:55 -0800

    updating starter code so backend functions properly

--- backend/config.js
+++ backend/config.js
@@ -10,10 +10,18 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
-function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "jobly_test"
+//       : process.env.DATABASE_URL || "jobly";
+// }
+
+let DB_NAME;
+
+if (process.env.NODE_ENV === "test") {
+  DB_NAME = 'jobly_test';
+} else {
+  DB_NAME = 'jobly';
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -25,12 +33,13 @@ console.log("Jobly Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
+// console.log("Database:".yellow, getDatabaseUri());
+console.log("Database:".yellow, DB_NAME);
 console.log("---");
 
 module.exports = {
   SECRET_KEY,
   PORT,
   BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
+  DB_NAME
 };
--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,27 @@
 "use strict";
 /** Database setup for jobly. */
 const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
+const { getDatabaseUri, DB_NAME } = require("./config");
 
-let db;
+// let db;
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri()
+//   });
+// }
+
+const db = new Client({
+  host: "/var/run/postgresql/",
+  database: DB_NAME
+});
 
 db.connect();
 
