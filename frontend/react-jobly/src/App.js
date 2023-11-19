2fce188c86bde08aaad63b5e3429a40e87873e6e
RQuicho <raymondquicho@gmail.com>
2023-11-18 21:33:38 -0800

    updating NavBar.js and creating useLocalStorage hook.

--- frontend/react-jobly/src/App.js
+++ frontend/react-jobly/src/App.js
@@ -4,11 +4,34 @@ import {BrowserRouter} from 'react-router-dom';
 import RoutePaths from './RoutePaths';
 import NavBar from './NavBar';
 import JoblyApi from './api';
+import jwt from 'jsonwebtoken';
+import UserContext from './UserContext';
+import useLocalStorage from './hooks/useLocalStorage';
+
 
 function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState(JoblyApi.token);
   const [currentUser, setCurrentUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      if (token) {
+        try {
+          let {username} = jwt.decode(token);
+          JoblyApi.token = token;
+          let currentUser = await JoblyApi.getUser(username);
+          setCurrentUser(currentUser);
+        } catch(e) {
+          console.error('Unable to get user info in App.js', e);
+          setCurrentUser(null);
+        }
+      }
+      setIsLoading(false);
+    }
+    setIsLoading(true);
+    getUserInfo();
+  }, [token]);
 
   const login = async (loginData) => {
     try {
@@ -39,13 +62,19 @@ function App() {
 
 
 
+  if(isLoading) {
+    return <p>Loading &hellip;</p>
+  }
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <NavBar logout={logout}/>
-        <RoutePaths login={login} signup={signup}/>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{currentUser, setCurrentUser}}>
+        <div className="App">
+          <NavBar logout={logout}/>
+          <RoutePaths login={login} signup={signup}/>
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
--- frontend/react-jobly/src/NavBar.js
+++ frontend/react-jobly/src/NavBar.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import {Navbar, Nav, NavItem} from 'reactstrap';
+import UserContext from './UserContext';
 
-const NavBar = () => {
-  return (
-    <div>
+const NavBar = ({logout}) => {
+  const {currentUser} = useContext(UserContext);
+
+  const navLoggedIn = () => {
+    return (
       <Navbar expand="md" color='light'>
         <NavLink to='/' className="navbar-brand">
           Jobly
         </NavLink>
-
-        {/* Show links to login and signup forms depending on if user is logged in */}
-
         <Nav className='ml-auto' navbar>
           <NavItem>
             <NavLink to='/companies'>Companies</NavLink>
@@ -19,10 +19,40 @@ const NavBar = () => {
           <NavItem>
             <NavLink to='/jobs'>Jobs</NavLink>
           </NavItem>
+          <NavItem>
+            <NavLink to='/' onClick={logout}>
+              Logout {currentUser.username}
+            </NavLink>
+          </NavItem>
         </Nav>
       </Navbar>
+    );
+  }
+
+  const navLoggedOut = () => {
+    return (
+      <Navbar expand="md" color='light'>
+        <NavLink to='/' className="navbar-brand">
+          Jobly
+        </NavLink>
+        <Nav className='ml-auto' navbar>
+          <NavItem>
+            <NavLink to='/login'>Login</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to='/signup'>Signup</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    );
+  }
+
+  return (
+    <div>
+      {currentUser ? navLoggedIn() : navLoggedOut()}
     </div>
-  )
+  );
+
 }
 
 export default NavBar;
