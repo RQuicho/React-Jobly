9b80f75d587a8d8f6d591b4e11b56873c8fe66c7
RQuicho <raymondquicho@gmail.com>
2023-11-18 22:45:47 -0800

    adding applied button

--- frontend/react-jobly/src/jobs/JobCard.js
+++ frontend/react-jobly/src/jobs/JobCard.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import UserContext from '../UserContext';
 
-const JobCard = ({job}) => {
+const JobCard = ({id, job}) => {
+  const {hasAppliedToJob, applyToJob} = useContext(UserContext);
+  const [applied, setApplied] = useState();
+
+  useEffect(() => {
+    setApplied(hasAppliedToJob(id));
+  }, [id, hasAppliedToJob]);
+
+  const handleApply = async (e) => {
+    if (hasAppliedToJob(id)) return;
+    applyToJob(id);
+    setApplied(true);
+  }
+
   return (
     <div>
       <p>{job.title}</p>
       <p>{job.companyName}</p>
       <p>Salary: {job.salary}</p>
       <p>Equity: {job.equity}</p>
+      <button
+        onClick={handleApply}
+        disabled={applied}
+      >
+        {applied ? "Applied" : "Apply"}
+      </button>
     </div>
   )
 }
