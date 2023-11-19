93f26c6004441bffcf96ed1cc6b255c167498c4d
RQuicho <raymondquicho@gmail.com>
2023-11-18 17:00:28 -0800

    adding login, logout, signup functions in App.js

--- frontend/react-jobly/src/App.js
+++ frontend/react-jobly/src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import RoutePaths from './RoutePaths';
 import NavBar from './NavBar';
+import JoblyApi from './api';
 
 function App() {
+  const [token, setToken] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const login = async (loginData) => {
+    try {
+      let token = await JoblyApi.login(loginData);
+      setToken(token);
+      return {success: true};
+    } catch(e) {
+      console.error('login failed', e);
+      return {success: false, e};
+    }
+  }
+
+  const logout = () => {
+    setCurrentUser(null);
+    setToken(null);
+  }
+
+  const signup = async (signupData) => {
+    try {
+      let token = await JoblyApi.signup(signupData);
+      setToken(token);
+      return {success: true};
+    } catch(e) {
+      console.error('signup failed', e);
+      return {success: false, e};
+    }
+  }
+
+
+
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <RoutePaths />
+        <NavBar logout={logout}/>
+        <RoutePaths login={login} signup={signup}/>
       </BrowserRouter>
     </div>
   );
